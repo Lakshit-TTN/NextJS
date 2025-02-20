98a1cabb8c7d0a0fb61fb439b1a3eb1c4fa7d4fb
Lakshit-TTN <lakshit@tothenew.com>
2025-02-20 21:26:21 +0530

    assognment - 1 (app routing)

---
+++ next-app-router/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="bg-gray-800 text-white absolute bottom-0 w-full text-center py-4">
+        <p> &copy; Terms & Conditions</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
